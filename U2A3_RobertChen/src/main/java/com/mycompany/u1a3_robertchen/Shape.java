7291214fa963771b2cfbaecab3c3c1b914c11beb
335091559 <335091559@C-2UA4162GS4.tdsb.on.ca>
2023-11-23 11:41:35 -0500

    Added all the elements to the JForm

---
+++ U2A3_RobertChen/src/main/java/com/mycompany/u1a3_robertchen/Shape.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.u1a3_robertchen;
+
+/**
+ *
+ * @author 335091559
+ */
+public class Shape {
+    private double dimension1;
+    private double dimension2;
+    private double dimension3;
+    private double dimension4;
+    public Shape (double dimension1, double dimension2, double dimension3, double dimension4) {
+        this.dimension1 = dimension1;
+        this.dimension2 = dimension2;
+        this.dimension3 = dimension3;
+        this.dimension4 = dimension4;
+    }
+}
